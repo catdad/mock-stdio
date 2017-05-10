04b4bc8c6d90e83e1f9d21a5fd76a02c6a6349e8
Kiril Vatev <vatev.1@gmail.com>
2017-05-10 17:51:29 -0400

    fixing indentation

--- index.js
+++ index.js
@@ -4,38 +4,38 @@
 // test values and not log during a test
 var fakeIo = (function () {
 
-    var originalStdout = process.stdout.write;
-    var originalStderr = process.stderr.write;
+  var originalStdout = process.stdout.write;
+  var originalStderr = process.stderr.write;
 
-    var outData = [];
-    var errData = [];
+  var outData = [];
+  var errData = [];
 
-    function collect(arr) {
-        return function (val) {
-            arr.push(new Buffer(val));
-        };
-    }
-
-    return {
-        activate: function () {
-            process.stdout.write = collect(outData);
-            process.stderr.write = collect(errData);
-        },
-        deactivate: function () {
-            process.stdout.write = originalStdout;
-            process.stderr.write = originalStderr;
-
-            var data = {
-                stdout: Buffer.concat(outData).toString(),
-                stderr: Buffer.concat(errData).toString()
-            };
-
-            outData = [];
-            errData = [];
-
-            return data;
-        }
+  function collect(arr) {
+    return function (val) {
+      arr.push(new Buffer(val));
     };
+  }
+
+  return {
+    activate: function () {
+      process.stdout.write = collect(outData);
+      process.stderr.write = collect(errData);
+    },
+    deactivate: function () {
+      process.stdout.write = originalStdout;
+      process.stderr.write = originalStderr;
+
+      var data = {
+        stdout: Buffer.concat(outData).toString(),
+        stderr: Buffer.concat(errData).toString()
+      };
+
+      outData = [];
+      errData = [];
+
+      return data;
+    }
+  };
 })();
 
 module.exports = fakeIo;
