82bd1e4cf578ac5c67e51a2daa105d13f7ad036d
Kiril Vatev <vatev.1@gmail.com>
2017-05-10 17:55:35 -0400

    a whole bunch of cleanup

--- index.js
+++ index.js
@@ -1,41 +1,34 @@
 /* jshint node: true */
 
-// allow hijacking IO, so that we can both
-// test values and not log during a test
-var fakeIo = (function () {
+var originalStdout = process.stdout.write;
+var originalStderr = process.stderr.write;
 
-  var originalStdout = process.stdout.write;
-  var originalStderr = process.stderr.write;
+var outData = [];
+var errData = [];
 
-  var outData = [];
-  var errData = [];
-
-  function collect(arr) {
-    return function (val) {
-      arr.push(new Buffer(val));
-    };
-  }
-
-  return {
-    activate: function () {
-      process.stdout.write = collect(outData);
-      process.stderr.write = collect(errData);
-    },
-    deactivate: function () {
-      process.stdout.write = originalStdout;
-      process.stderr.write = originalStderr;
-
-      var data = {
-        stdout: Buffer.concat(outData).toString(),
-        stderr: Buffer.concat(errData).toString()
-      };
-
-      outData = [];
-      errData = [];
-
-      return data;
-    }
+function collect(arr) {
+  return function (val) {
+    arr.push(new Buffer(val));
   };
-})();
+}
 
-module.exports = fakeIo;
+module.exports = {
+  activate: function () {
+    process.stdout.write = collect(outData);
+    process.stderr.write = collect(errData);
+  },
+  deactivate: function () {
+    process.stdout.write = originalStdout;
+    process.stderr.write = originalStderr;
+
+    var data = {
+      stdout: Buffer.concat(outData).toString(),
+      stderr: Buffer.concat(errData).toString()
+    };
+
+    outData = [];
+    errData = [];
+
+    return data;
+  }
+};
