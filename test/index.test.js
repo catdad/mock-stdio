708eac739473c258d8ca8d71e1024a6ddc10a18c
Kiril Vatev <vatev.1@gmail.com>
2017-05-10 17:43:51 -0400

    adding more tests

--- test/index.test.js
+++ test/index.test.js
@@ -8,4 +8,53 @@ describe('[index]', function () {
   it('has two methods', function () {
     expect(lib).to.have.all.keys(['activate', 'deactivate']);
   });
+
+  it('collects stdout data from process.stdout stream', function () {
+    var str = 'banana';
+
+    lib.activate();
+
+    process.stdout.write(str);
+
+    var data = lib.deactivate();
+
+    expect(data.stdout).to.equal(str);
+  });
+
+  it('collects stdout data from console.log', function () {
+    var str = 'banana';
+
+    lib.activate();
+
+    console.log(str);
+
+    var data = lib.deactivate();
+
+    expect(data.stdout).to.equal(str + '\n');
+  });
+
+  it('collects stderr data from process.stderr stream', function () {
+    var str = 'banana';
+
+    lib.activate();
+
+    process.stderr.write(str);
+
+    var data = lib.deactivate();
+
+    expect(data.stderr).to.equal(str);
+  });
+
+  it('collects stdout data from console.error', function () {
+    var str = 'banana';
+
+    lib.activate();
+
+    console.error(str);
+
+    var data = lib.deactivate();
+
+    expect(data.stderr).to.equal(str + '\n');
+  });
+
 });
